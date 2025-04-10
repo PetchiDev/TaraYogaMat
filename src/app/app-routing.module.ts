4122517bdb33a54ce0e432fc8dad7663224412fb
Petchiappan.P <Petchiappan.P@kryptosinfosys.com>
2025-04-10 23:02:49 +0530

    Upto KnowSection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'taraYogaMat', pathMatch: 'full' },
+  { path: 'taraYogaMat', component: LandingPageComponent }];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
